8f3961a53cec3051df5eafbc04222de867c8d55d
christina holland <cholland369@gmail.com>
2024-07-22 15:08:46 -0400

    Added assignment files

---
+++ src/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My Name is");
+        System.out.println("Christina");
+        System.out.println("Holland");
+    }
+}
